df457292552a85878d60dd1935d1509b4d33758e
wberrari <wassim.berrari@etu.u-bordeaux.fr>
2018-11-19 21:34:23 +0100

    PART III

--- code/threads/.main.d
+++
@@ -1,4 +0,0 @@
-.main.d main.o: main.cc copyright.h utility.h ../threads/bool.h \
- ../machine/sysdep.h ../threads/copyright.h synch.h thread.h list.h \
- system.h scheduler.h ../machine/interrupt.h ../threads/list.h \
- ../machine/stats.h ../machine/timer.h ../threads/utility.h
